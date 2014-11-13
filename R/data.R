591eba61fb5d7066e5a21abfd89cfd4a31c758a6
Jarad Niemi <niemi@iastate.edu>
2014-11-13 14:19:19 -0600

    added data set

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Heterosis dataset
+#' 
+#' A dataset containing two parental genotypes (B & M)
+#' and both crosses (BM & MB). 
+#'  
+#' \itemize{
+#'   \item gene_id. the gene identification
+#'   \item block. the block that the observation came from
+#'   \item genotype. the genotype of the sample
+#'   \item source. whether the read can be attributed to the b or m parental genotype or the total count for that gene
+#'   \item count. the RNAseq read count
+#' }
+#' 
+#' @format A data frame with 1903488 rows and 10 columns
+#' @name heterosis
+#' NULL
