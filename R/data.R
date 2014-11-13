b18eb3b8ab82d7a883ab887b4de0423897a92673
Jarad Niemi <niemi@iastate.edu>
2014-11-13 14:44:31 -0600

    added chip column

--- R/data.R
+++ R/data.R
@@ -4,10 +4,11 @@
 #' and both crosses (BM & MB). 
 #'  
 #' \itemize{
-#'   \item gene_id. the gene identification
-#'   \item block. the block that the observation came from
-#'   \item genotype. the genotype of the sample
-#'   \item source. whether the read can be attributed to the b or m parental genotype or the total count for that gene
+#'   \item gene_id. factor. the gene identification
+#'   \item block. factor. the block that the observation came from
+#'   \item chip. factor. the RNAseq chip that was used 
+#'   \item genotype. factor. the genotype of the sample
+#'   \item source. factor. whether the read can be attributed to the b or m parental genotype or the total count for that gene
 #'   \item count. the RNAseq read count
 #' }
 #' 
